d19668536dae2abd8ceb9fca9fc269fc9d6444ac
Steven Hall <steven.hall@sendtonews.com>
2021-04-09 16:44:20 -0700

    Enable emojis for dmenu

--- config.def.h
+++ config.def.h
@@ -6,16 +6,32 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 	static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#928374", "#282828" },
-	[SchemeSel] = { "#282828", "#458588" },
-	[SchemeSelHighlight] = { "#fabd2f", "#458588" },
-	[SchemeNormHighlight] = { "#d75f5f", "#222222" },
-	[SchemeOut] = { "#ff0000", "#ff0000" },
+	[SchemeNorm] = {
+		"#928374",
+		"#282828"
+	},
+	[SchemeSel] = {
+		"#282828",
+		"#458588"
+	},
+	[SchemeSelHighlight] = {
+		"#fabd2f",
+		"#458588"
+	},
+	[SchemeNormHighlight] = {
+		"#d75f5f",
+		"#222222"
+	},
+	[SchemeOut] = {
+		"#ff0000",
+		"#ff0000"
+	},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
