ec85780d996fb66d2e2ede2a9d69f2fce31e9498
Steven Hall <Hallzy.18@gmail.com>
2021-04-10 09:45:07 -0700

    Increase emoji size for dmenu

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static int min_width = 1000;                    /* minimum width when centered *
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
-	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
+	"Noto Color Emoji:pixelsize=22:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 	static const char *colors[SchemeLast][2] = {
